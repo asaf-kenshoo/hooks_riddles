04e52a7655f6c4fb523fbbb1dc963389523e2cb9
asaf-kenshoo <asaf.levy@kenshoo.com>
2020-07-02 10:42:52 +0300

    starting riddle

---
+++ DNA/test/hooks/riddle/DnaStrandTest.java
@@ -0,0 +1,20 @@
+package hooks.riddle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DnaStrandTest {
+  @Test
+  public void test01() {
+    assertEquals("TTTT", DnaStrand.makeComplement("AAAA"));
+  }
+  @Test
+  public void test02() {
+    assertEquals("TAACG", DnaStrand.makeComplement("ATTGC"));
+  }
+  @Test
+  public void test03() {
+    assertEquals("CATA", DnaStrand.makeComplement("GTAT"));
+  }
+}
---
+++ XO/src/hooks/riddle/XO.java
@@ -0,0 +1,22 @@
+package hooks.riddle;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contains any char.
+Examples input/output:
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+
+public class XO {
+
+  public static boolean getXO(String str) {
+
+  }
+
+}
