21345a206d14e47081edd2fee062eac3b0063c9f
asaf-kenshoo <asaf.levy@kenshoo.com>
2020-07-05 17:47:16 +0300

    adding solution for sum_of_tow

--- SUM_OF_TWO/src/hooks/riddle/SUM_OF_TOW.java
+++ SUM_OF_TWO/src/hooks/riddle/SUM_OF_TOW.java
@@ -16,12 +16,42 @@ Output: -1
 No valid pair exists.
 */
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SUM_OF_TOW {
 
-    public static boolean hasArrayTwoCandidates(int A[], int sum) {
+//    public static boolean hasArrayTwoCandidates(int A[], int sum) {
+//        int l, r, arr_size;
+//
+//        /* Sort the elements */
+//        Arrays.sort(A);
+//
+//        l = 0;
+//        arr_size = A.length;
+//        r = arr_size - 1;
+//        while (l < r) {
+//            int sumOfTwo = A[l] + A[r];
+//            if (sumOfTwo == sum)
+//                return true;
+//            else if (sumOfTwo < sum)
+//                l++;
+//            else
+//                r--;
+//        }
+//        return false;
+//    }
 
+    public static boolean hasArrayTwoCandidates(int A[], int sum) {
+        Set<Integer> hashSet = new HashSet<Integer>();
+        for (int i = 0; i < A.length ; i++) {
+            var temp = sum - A[i];
+            if (hashSet.contains(temp)) {
+                return true;
+            } else {
+                hashSet.add(A[i]);
+            }
+        }
         return false;
     }
 
